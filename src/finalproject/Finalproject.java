ed6750fdc6005efeef848ecfbde71de6cd3a2143
Beatrice Wangui <38465763+Ndongar@users.noreply.github.com>
2018-12-14 21:16:28 +0300

    Add files via upload

---
+++ src/finalproject/Finalproject.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package finalproject;
+
+import java.awt.Button;
+import javax.swing.JFrame;
+
+
+/**
+ *
+ * @author hp
+ */
+public class Finalproject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+       B bingo = new B();
+       bingo.setVisible(true);
+        
+    }
+    
+}
